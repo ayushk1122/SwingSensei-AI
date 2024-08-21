e6e99b913f2d2aa103a5b5a27dbe162820e83fb3
Ayush Krishnappa <akrishnappa@businessolver.com>
2024-08-21 15:15:45 -0700

    Initial Commit

---
+++ SwingSensei AI/SwingSensei_AIApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwingSensei_AIApp.swift
+//  SwingSensei AI
+//
+//  Created by Ayush Krishnappa on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwingSensei_AIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
